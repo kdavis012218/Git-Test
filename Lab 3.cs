16e69fbebc0de807b115523ee06d0bb567b1f342
Kevin Davis <35358468+kdavis012218@users.noreply.github.com>
2018-01-25 01:49:34 -0500

    Add files via upload

---
+++ Lab 3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("       BOOT CAMP LAB 3 "     );
+            Console.WriteLine("       BY KEVIN DAVIS ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            {
+
+                int x = 0;
+                Console.WriteLine("Number  Square   Cube");
+                Console.WriteLine("{0}{1}{2}{3}{4}",  x, "      ", Math.Pow(x,2),    "          ", Math.Pow(x,3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+                Console.WriteLine("{0}{1}{2}{3}{4}",  ++x, "      ", Math.Pow(x, 2), "          ", Math.Pow(x, 3));
+
+
+
+
+
+            }
+            {
+                
+            }
+        }
+    }
+
+}
+   
+    
+
