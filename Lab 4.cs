0030cc39c2b0b0c2e0c70b31e6305669232af680
Kevin Davis <35358468+kdavis012218@users.noreply.github.com>
2018-01-25 23:26:40 -0500

    Add files via upload

---
+++ Lab 4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, number, factorial;
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("       BOOT CAMP LAB 4 ");
+            Console.WriteLine("     FACTORIAL CALCULATOR ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Enter an integer greater than 0 but less than ten");
+            number = int.Parse(Console.ReadLine());
+            factorial = number;
+            for (a = number - 1; a >= 1; a--)
+            {
+                factorial = factorial * a;
+            }
+            Console.WriteLine("\nFactorial is: " + factorial);
+            bool run = true;
+            int n = 0;
+            while (run == true)
+
+            {
+
+                Console.WriteLine(n);
+                n++;
+                if (n >= 10)
+                    break;
+                {
+                    Console.WriteLine("Would you like to Continue y or n?");
+                    string response1 = Console.ReadLine();
+                    if (response1 == "y")
+                        continue;
+
+                    else
+                    {
+                        break;
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+       
---
+++ lab test 5.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("       BOOT CAMP LAB 5 ");
+            Console.WriteLine("      HI-ROLLER DICE SIM ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Random randNum = new Random();
+            int rollNum = 1;
+            int die1;
+            int die2;
+            int response;
+            for (rollNum = 1; rollNum < 20; rollNum++)
+            {
+                Console.WriteLine("Enter Number Of Sides");
+                response = Convert.ToInt32(Console.ReadLine());
+                if (response < 2)
+                    break;
+                
+
+                die1 = randNum.Next(minValue: 1, maxValue:response);
+                die2 = randNum.Next(minValue: 1, maxValue: response);
+                Console.WriteLine(value: "Roll " + rollNum + ": " + die1 + " " + die2);
+                if (die1==die2)
+                    Console.Write("Double UP! ");
+                if (die1 == 1 && die2 == 1)
+                    Console.Write("SNAKE EYES");
+
+                    
+                    Console.WriteLine("Would you like to roll again?");
+                    string response1 = Console.ReadLine();
+                if (response1 == "n")
+                    break;
+                    {
+                        
+                        
+                    }
+                    {
+
+                    }
+                   
+
+
+
+
+
+                }
+                { }
+            }
+        }
+    }
+
+    
+
