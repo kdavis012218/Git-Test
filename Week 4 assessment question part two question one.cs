9fe5c27460ac4ec37150d12c17c9836f235cc8a7
Kevin Davis <35358468+kdavis012218@users.noreply.github.com>
2018-02-15 10:58:28 -0500

    Add files via upload
    code question one

---
+++ Week 4 assessment question part two question one.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { }
+
+        private class Car
+        {
+            internal class Name
+            {
+                string make;
+                Car a = new Car();
+                new Car.Name = new Name();
+                Name = "Esprit";
+                return(Name)
+            }
+        }
+      
+        internal class VIN
+        {
+            private int VIN;
+            VIN a = new VIN();
+            new VIN = V();
+            VIN = "12345";
+            return(VIN)
+        }
+        
+    }
+        
+    {
+
+    }
+
+    }
+
+        class Nascar
+        {
+            
+            
+         private class TeamName
+    {
+        Nascar.TeamName a = new TeamName();
+        new Nascar.TeamName = "Rockets";
+            return(TeamName)
+    }
+}
+
+        }
+
+    }
+}
